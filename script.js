9d91523fe60fc21e13b9b6236a969980eb8d3f58
MarjitaSaha <103816611+MarjitaSaha@users.noreply.github.com>
2023-05-05 10:35:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+// Get the container element
+const container = document.querySelector('.container');
+
+// Create 800 boxes dynamically
+for (let i = 0; i < 800; i++) {
+	const box = document.createElement('div');
+	box.classList.add('square');
+	container.appendChild(box);
+
+	// Add event listener to change the background color on hover
+	box.addEventListener('mouseover', () => {
+		box.style.backgroundColor = 'yellow';
+	});
+
+	// Add event listener to revert the background color after 1 second
+	box.addEventListener('mouseout', () => {
+		setTimeout(() => {
+			box.style.backgroundColor = 'white';
+		}, 1000);
+	});
+}
